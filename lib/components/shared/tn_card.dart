63d3405decf7048deda3e9028e6714bc70df86c4
smirk1795 <hardikpatel.hardik36@gmail.com>
2018-05-25 03:55:09 -0400

    Add Read More to launch url.

--- lib/components/shared/tn_card.dart
+++ lib/components/shared/tn_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TNCard extends StatelessWidget {
   final AsyncSnapshot snapshot;
@@ -6,8 +7,10 @@ class TNCard extends StatelessWidget {
 
   TNCard(this.snapshot, this.position);
 
-  _launchUrl(String url) {
-
+  _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
   }
 
   @override
@@ -30,29 +33,36 @@ class TNCard extends StatelessWidget {
                     new Container(
                       margin: new EdgeInsets.only(top: 10.0),
                       alignment: Alignment.topLeft,
-                      child: new Text(
-                          "${snapshot.data[position].publishedAt}",
-                          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, color: Colors.grey.shade400)),
-                    ),
-                    new Container(
-                      margin: new EdgeInsets.only(top: 10.0),
-                      child: new InkWell(
-                        child: new Row(
-                          children: <Widget>[
-                            new Icon(Icons.info_outline, color: Colors.lightBlueAccent, size: 16.0),
-                            new Padding(padding: new EdgeInsets.only(left: 4.0)),
-                            new Text("Read More", style: new TextStyle(fontSize: 16.0, color: Colors.lightBlueAccent)),
-                          ],
-                        ),
-                        onTap: () => _launchUrl(snapshot.data[position].url),
-                      ),
+                      child: new Row(
+                        children: <Widget>[
+                          new Expanded(
+                            child: new Text(
+                                "${snapshot.data[position].publishedAt}",
+                                style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, color: Colors.grey.shade400)),
+                          ),
+                          new Expanded(
+                            child: new Container(
+                              child: new InkWell(
+                                child: new Row(
+                                  children: <Widget>[
+                                    new Icon(Icons.info_outline, color: Colors.lightBlueAccent, size: 16.0),
+                                    new Padding(padding: new EdgeInsets.only(left: 4.0)),
+                                    new Text("Read More", style: new TextStyle(fontSize: 16.0, color: Colors.lightBlueAccent)),
+                                  ],
+                                ),
+                                onTap: () => _launchUrl(snapshot.data[position].url),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
                     ),
                   ],
                 )
               ),
             ),
             new Container(
-              height: 130.0,
+              height: 140.0,
               width: 140.0,
               child: snapshot.data[position].urlToImage != null ?
               new Image.network(snapshot.data[position].urlToImage,fit: BoxFit.fill, alignment: Alignment.topLeft) :
